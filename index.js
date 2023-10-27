fd9c7edf212e467ced9fb6897247a9ca94deacb6
Ana <anmeuri@gmail.com>
2023-10-26 22:49:05 -0700

    Implemented the functions for index.js and iss.js so it fetches our IP address

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+const { fetchMyIP } = require('./iss');
+
 // Main fetch function
 
-const fetchInfo = function () {};
+fetchMyIP((error, ip) => {
+  if (error) {
+    console.log("It didn't work", error);
+    return;
+  }
+
+  console.log("It worked! Returned IP:", ip);
+});
